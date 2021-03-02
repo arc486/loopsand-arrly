26dbf343098db6fb700789ca2d5f5d4fa51572a4
arc486 <77684570+arc486@users.noreply.github.com>
2021-03-02 13:28:10 -0500

    first commit

---
+++ script2.js
@@ -0,0 +1,39 @@
+// log from 0 to 1000
+ let count = 0;
+while(count <= 1000) {
+    console.log(count);
+    count++;
+ }
+for ( let i = 0; i <= 1000; i++) {
+    console.log(i); } 
+
+while(count <= 10000) {
+    console.log(count);
+    count++;
+}
+alert("A quarter of the way there!")
+
+alert("Halfway there!")
+
+alert( "the loop is done"  )
+
+
+let x = 5;
+const y = 10;
+
+
+
+const favoritetvshows = ["doctor who","wandavision","pokemon","yugioh","supernatural"];
+
+
+
+console.log(favoritetvshows);
+
+for(let i = 0; i < favoritetvshows.length; i++) {
+    console.log(favoritetvshows[i]) 
+}
+
+
+    
+
+    
